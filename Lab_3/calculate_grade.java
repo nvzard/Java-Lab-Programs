fd0b0c0beaad163525564fdcba68c1d419447956
nvzard <nitanshu.vzard@gmail.com>
2017-08-27 13:19:00 +0530

    Added Lab 3 Q1

---
+++ Lab_3/calculate_grade.java
@@ -0,0 +1,66 @@
+import java.io.*;
+
+class Student{
+
+    double physics, chemistry, maths;
+    private String name;
+    private int age;
+    Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    void setMaths(double m){
+        this.maths = m;
+    }
+
+    void setPhysics(double p){
+        this.physics = p;
+    }
+
+    void setChemistry(double c){
+        this.chemistry = c;
+    }
+
+    void grade()
+    {
+        double avg = (physics+chemistry+maths)/3;
+
+        if(avg >= 90)
+            System.out.println("Grade is A++");
+        else if(avg >= 80 && avg<90)
+            System.out.println("Grade is A+");
+        else if(avg >= 70 && avg<80)
+            System.out.println("Grade is A");
+        else if(avg >= 60 && avg<70)
+            System.out.println("Grade is B++");
+        else if(avg >= 50 && avg<60)
+            System.out.println("Grade is B+");
+        else if(avg >= 40 && avg<50)
+            System.out.println("Grade is B");
+        else if(avg >= 35 && avg<40)
+            System.out.println("Grade is C");
+        else if(avg < 35)
+            System.out.println("Grade is F");
+        
+    }
+
+}
+
+class Driver{
+    
+    public static void main(String[] args){
+    
+        Student greg = new Student("Greg", 20);
+        
+        greg.setPhysics(70);
+        greg.setChemistry(85);
+        greg.setMaths(60);
+        greg.grade();
+
+    }
+}
+
+
+
+
