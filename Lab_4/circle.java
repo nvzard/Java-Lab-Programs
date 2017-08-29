1870a42f3f065e5892ae7bfe02ae35ca13965a56
nvzard <nitanshu.vzard@gmail.com>
2017-08-29 16:44:59 +0530

    Minor changes

--- Lab_4/circle.java
+++ Lab_4/circle.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.text.DecimalFormat;
 
 class Circle{
     final double pi =22/7;
@@ -24,11 +25,13 @@ class Circle{
 class Driver{
     
     public static void main(String[] args){
+        
+       // DecimalFormat dc = new DecimalFormat(".##");
         Circle obj = new Circle();
         obj.setRadius(10.956);
-        System.out.println("Circumference: "+ obj.circleCircumference(10));
+        System.out.println("Circumference: "+obj.circleCircumference(10));
         System.out.println("Area of radi 10.956: "+obj.calculateArea());
-        System.out.println("Arc length of 45 degree: "+ obj.arcLength(45));
+        System.out.println("Arc length of 45 degree: "+obj.arcLength(45));
 
     }
 }
