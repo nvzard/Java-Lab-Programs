28e269672408568341c758fe77269520442f031d
nvzard <nitanshu.vzard@gmail.com>
2017-08-31 23:01:45 +0530

    Readme update

---
+++ Class_Room_Programs/nested_class.java
@@ -0,0 +1,41 @@
+
+class Outer{
+
+    void outerMethod(){
+        obj.innerMethod();
+        InnerStatic.innerStaticMethod();
+    }
+    
+    int x =57;
+    static int s = 99;
+
+    class Inner{
+               
+        void innerMethod(){
+            System.out.println("Inner method "+x);
+        } 
+   
+    }
+
+    static class InnerStatic{
+                
+        static void innerStaticMethod(){
+            System.out.println(s);
+        }
+
+    }
+    Inner obj = new Inner(); 
+
+}
+
+
+class Driver{
+
+    public static void main(String[] args){
+
+        Outer obj = new Outer();
+
+        obj.outerMethod();
+    }
+}
+
