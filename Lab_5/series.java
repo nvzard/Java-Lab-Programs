e1bbe2df74b81dc1d27d4fcbb051787a3f19369c
nvzard <nitanshu.vzard@gmail.com>
2017-09-02 12:04:54 +0530

    Added sum of series Lab 5

---
+++ Lab_5/series.java
@@ -0,0 +1,82 @@
+import java.lang.Math;
+
+class Series{
+
+    void series_1(int n){
+        double sum=0;
+        for(int i=1; i<=n; i++)
+            sum += 1/(double)i;
+        
+
+        System.out.println("Sum for "+n+" terms: "+sum);
+    }
+
+    void series_2(int n){
+        double sum = 1;
+        for(int i=1; i<n; i++)
+        {
+            if(n > 1){
+
+            sum += 1/Math.pow(2,i);
+            }else{
+                sum = 1;
+                break;
+            }
+        }
+        
+        System.out.println("Sum for "+n+" terms: "+sum);
+        }
+
+    int factorial(int n){
+        int fact = 1;
+        for(int i=n; i>0; i--)
+            fact *=i;
+        return fact;
+    }
+
+    void series_3(int n){
+        double sum=1;
+        for(int i=2; i<=n; i++){
+            if(n >1)
+                sum += (double)i/(double)factorial(i-1);
+            else{
+                sum = 1;
+                break;
+            }
+        }
+                
+
+        System.out.println("Sum of "+n+" terms: "+sum);
+    }
+
+}
+
+class Driver{
+    public static void main(String[] args){
+        
+        Series obj =new Series();
+
+        obj.series_1(2);
+        obj.series_2(3);
+        obj.series_3(3);
+//        System.out.println(obj.factorial(1)); 
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
