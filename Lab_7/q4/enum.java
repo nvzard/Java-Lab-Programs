e65b70fae2a5f0e48d022b471662ac4f1ed901ab
nvzard <nitanshu.vzard@gmail.com>
2017-09-25 13:56:15 +0530

    Q4 added

---
+++ Lab_7/q4/enum.java
@@ -0,0 +1,13 @@
+class Enum{
+    
+    enum Seasons { WINTER, SPRING, SUMMER, FALL}
+
+    public static void main(String args[]){
+
+        Seasons s = Seasons.WINTER;
+
+        System.out.println(s);
+    }
+
+
+}
