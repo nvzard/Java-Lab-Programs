351d8e3308a92e0e6f3a2d9af8f2df02a01512f7
nvzard <nitanshu.vzard@gmail.com>
2017-10-10 16:03:32 +0530

    Added Lab 10

--- Lab_10/q1/q1.java
+++ Lab_10/q1/q1.java
@@ -6,12 +6,31 @@ class Driver{
     public static void main(String args[]){
 
     String sentence = "If you are good at something never do it for free";
-    String exp = "10 + 20 + 40 / 60";
+    String expression = "10 + 2 + 40 / 60";
 
     StringTokenizer sent = new StringTokenizer(sentence, " ");
 
+    System.out.println("Sentence:\n");
+    System.out.println("No of Tokens: "+ sent.countTokens());
     while(sent.hasMoreTokens())
         System.out.println(sent.nextToken());
+    
 
+/*
+    StringTokenizer expression = new StringTokenizer(exp," ");
+    
+    while(expression.hasMoreTokens()){
+        if(expression.nextToken() == (int) expression.nextToken())
+            System.out.println(expression.nextToken());
+    }
+
+*/
+
+    StringTokenizer exp = new StringTokenizer(expression," ");
+    System.out.println("\nExpression:\n");
+    System.out.println("No of Tokens: "+exp.countTokens());
+    while(exp.hasMoreTokens())
+        System.out.println(exp.nextToken());
+    
     }
 }
