235bdd9c7c6839a1ee7dc0092bd7b19f143e5d36
Nitanshu <nitanshu.vzard@gmail.com>
2017-11-06 21:49:26 +0530

    Added Lab_12 programs

---
+++ Lab_12/q1_q2.java
@@ -0,0 +1,70 @@
+class InvalidRange extends Exception{
+    String str;
+
+    InvalidRange(String str){
+        super(str);
+    }
+}
+
+class CheckArgument extends Exception{
+    String str;
+
+    CheckArgument(String str){
+        super(str);
+    }
+}
+
+class Student{
+    String name;
+    int p, c, m, e, cs;
+
+    Student(String name){
+	this.name = name;
+    }
+
+    void inputMarks(int...marks){
+        int sum = 0;
+
+        try{
+
+            if(marks.length != 5){
+                CheckArgument arg = new CheckArgument("Only 5 arguments are allowed");
+                throw arg;
+            }
+        
+
+            for(int mark : marks){
+                if(mark < 0 || mark > 50){
+                    InvalidRange range = new InvalidRange("Range should be b/w 0-50");
+                 throw range;
+                }else
+                    sum += mark;
+            }
+
+            total(sum);
+        }
+        catch(CheckArgument arg){
+            System.out.println(arg);
+        }
+        catch(InvalidRange range){
+            System.out.println(range);
+        }
+
+    }
+    
+    void total(int sum){        
+        System.out.println("Total is: "+sum+" marks");
+        System.out.println("Percentage: "+ (sum*100)/(double)250 + "%");
+        System.out.println("Keep hustling "+name);
+    }
+
+}
+
+class Driver{
+    public static void main(String args[]){
+
+        Student st = new Student("Ram Dayal");
+        st.inputMarks(45,36,43,12,23);
+
+    }
+}
